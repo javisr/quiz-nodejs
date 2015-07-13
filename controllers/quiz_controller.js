278c1194709ae4dacc5f6b2b3bf064f0eebae3ac
Javier <javi@javisr.net>
2015-07-13 21:46:36 +0200

    cambia controlador

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,18 @@
 // GET /quizes/question
-exports.question = function (req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de Italia'});
+exports.question = function(req, res) {
+  res.render('quizes/question', {
+    pregunta: 'Capital de Italia'
+  });
 }
-exports.answer = function (req, res) {
-	if (req.query.respuesta === 'Roma') {
-		res.render('quizes/answer', {respuesta: 'Correcto'});
-	} else {
-		res.render('quizes/answer', {respuesta: 'Incorrecto'});
-	}
+exports.answer = function(req, res) {
+  var regExp = /^Roma$/i;
+  if (regExp.test(req.query.respuesta)) {
+    res.render('quizes/answer', {
+      respuesta: 'Correcto'
+    });
+  } else {
+    res.render('quizes/answer', {
+      respuesta: 'Incorrecto'
+    });
+  }
 }

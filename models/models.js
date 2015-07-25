f254deb7566f463f70a6255857e519415a5025eb
Javier <javi@javisr.net>
2015-07-25 16:25:31 +0200

    runnig in foreman with env vars

---
+++ models/models.js
@@ -0,0 +1,62 @@
+var path = require('path');
+
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = (url[6] || null);
+var user = (url[2] || null);
+var pwd = (url[3] || null);
+var protocol = (url[1] || null);
+var dialect = (url[1] || null);
+var port = (url[5] || null);
+var host = (url[4] || null);
+var storage = process.env.DATABASE_STORAGE;
+
+
+// Cargar Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite o Postgres
+var sequelize = new Sequelize(DB_name, user, pwd, {
+  dialect: protocol,
+  protocol: protocol,
+  port: port,
+  host: host,
+  storage: storage, // solo SQLite (.env)
+  omitNull: true // solo Postgres
+});
+
+
+// Importar la definicion de la tabla Quiz en quiz.js
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz; // exportar definición de tabla Quiz
+
+sequelize.sync()
+  .then(
+    function() { // ok(..) ejecuta el manejador una vez creada la tabla
+      Quiz.count()
+        .then(
+          function(count) {
+            if (count === 0) { // la tabla se inicializa solo si está vacía
+              Quiz.create({
+                  pregunta: 'Capital de Italia',
+                  respuesta: 'Roma'
+                })
+                .then(
+                  function() {
+                    console.log('Base de datos inicializada');
+                  },
+                  function(err) {
+                    console.log('Error create ' + err);
+                  }
+                );
+            }
+          },
+          function(err) {
+            console.log('Error count ' + err);
+          }
+        );
+    },
+    function(err) {
+      console.log('Error sync ' + err);
+    }
+  );
